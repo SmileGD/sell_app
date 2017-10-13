46224a6564ed48aad7209bce888cb844fc857351
wangzuowei2017 <wangzuowei@yunys.com.cn>
2017-10-13 18:25:42 +0800

    时间选择 地区选择 点中状态改变 完成

--- javascript/public.js
+++ javascript/public.js
@@ -1,36 +1,17 @@
-// window.$$=window.Zepto = Zepto;
-
 //搜索框
-// if($('.search').length > 0) {
-//     var search = $('.search')[0];
-//     var icon = $('.icon-search')[0];
-
-//     search.focus(function(){
-//         console.log(1);
-//         $(this).css("padding-left",'1rem');
-//         $(icon).css('left','0.6rem');
-//     })
-
-//     search.blur(function(){
-//         console.log(1);
-//         if($(this).val().length == 0){
-//             $(this).css("padding-left",'7.8rem');
-//             $(icon).css('left','7.4rem');
-//         }
-//     })
-// }
-var search = $('.search')[0];
-var icon = $('.icon-search')[0];
-search.onfocus = function(){
-    $(this).css("padding-left",'1rem');
-    $(icon).css('left','0.6rem');
+if($('.search').length > 0) {
+    $('.search').focus(function(){
+        $(this).css("padding-left",'1rem');
+        $('.icon-search').css('left','0.6rem');
+    })
+    $('.search').blur(function(){
+        if($(this).val().length == 0){
+            $(this).css("padding-left",'7.8rem');
+            $('.icon-search').css('left','7.4rem');
+        }
+    })
 }
-search.onblur = function(){
-    if($(this).val().length == 0){
-        $(this).css("padding-left",'7.8rem');
-        $(icon).css('left','7.4rem');
-    }
-}
+
 //弹出弹框
 $('.btn-announce').each(function(){
     $(this).on('click',function(){
@@ -38,31 +19,119 @@ $('.btn-announce').each(function(){
     })
 })
 
-// $('.n-new').each(function(){
-//     $(this).on('click',function(){
-//         $('#select-wrapper').css('display','block');
-//     })
-// })
-// $('#cancle').on('click',function(){
-//     $('#select-wrapper').css('display','none');
-// })
-// $('#sure').on('click',function(){
-//     $('#select-wrapper').css('display','none');
-// })
-
 //点击播放按钮 切换播放图标
-// $('.play').each(function(){
-//     $(this).on('click',function(){
-//         var i=$(this).find('i')[0];
-//     //     if(i.hasClass('icon-Triangle')){
-//     //         i.removeClass('icon-Triangle');
-//     //         i.addClass('icon-pause');
-//     //     }else{
-//     //         i.removeClass('icon-pause');
-//     //         i.addClass('icon-Triangle');
-//     //     }
-//     // })
-// })
+$('.play').each(function(){
+    $(this).on('click',function(){
+        var i=$(this).find('i')[0];
+        if($(i).hasClass('icon-Triangle')){
+            $(i).removeClass('icon-Triangle');
+            $(i).addClass('icon-pause');
+        }else{
+            $(i).removeClass('icon-pause');
+            $(i).addClass('icon-Triangle');
+        }
+    })
+})
+
+//点击选中勾选
+$('.info-check').each(function(){
+    $(this).on('click',function(){
+        $(this).find('i').toggle();
+    })
+})
+
+//轮播图
+if ($('script[src="./node_modules/swiper/dist/js/swiper.min.js"]').length > 0){
+    var swiper = new Swiper('.swiper-container',{
+        pagination: '.swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: 1500
+    });
+}
+
+if ($('script[src="./node_modules/zepto/dist/zepto.js"]').length > 0){
+    //zepto符号替换
+    window.$$=window.Zepto = Zepto;
+
+    $$("#date").calendar({
+        value: ['2017-01-01']
+    });
+    $$("#date-return").calendar({
+        value: ['2017-01-01']
+    });
+
+    if ($('script[src="./SUI-Mobile-dev/dist/js/sm-city-picker.min.js"]').length > 0){
+        $$("#city-picker").cityPicker({
+            toolbarTemplate: '<header class="bar bar-nav">\
+            <button class="button button-link pull-right close-picker">确定</button>\
+            </header>'
+        });
+    }
+
+    $$("#gender").picker({
+        toolbarTemplate: '<header class="bar bar-nav">\
+        <button class="button button-link pull-right close-picker">确定</button>\
+        </header>',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['男', '女', '保密']
+            }
+        ]
+    });
+    $$("#picker-1").picker({
+        toolbarTemplate: '<header class="bar bar-nav">\
+        <button class="button button-link pull-left close-picker">取消</button>\
+        <button class="button button-link pull-right close-picker">确定</button>\
+        </header>',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['1件', '2件', '3件', '4件', '5件', '6件', '7件', '8件', '9件', '10件', '11件', '12件', '13件', '14件', '15件', '16件', '17件', '18件', '19件', '20件']
+            }
+        ]
+    });
+    $$("#picker-2").picker({
+        toolbarTemplate: '<header class="bar bar-nav">\
+        <button class="button button-link pull-left close-picker">取消</button>\
+        <button class="button button-link pull-right close-picker">确定</button>\
+        </header>',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['5成新', '6成新', '7成新', '8成新', '9成新']
+            }
+        ]
+    });
+    $$("#picker-3").picker({
+        toolbarTemplate: '<header class="bar bar-nav">\
+        <button class="button button-link pull-left close-picker">取消</button>\
+        <button class="button button-link pull-right close-picker">确定</button>\
+        </header>',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['1件', '2件', '3件', '4件', '5件', '6件', '7件', '8件', '9件', '10件', '11件', '12件', '13件', '14件', '15件', '16件', '17件', '18件', '19件', '20件']
+            }
+        ]
+    });
+    $$("#picker-4").picker({
+        toolbarTemplate: '<header class="bar bar-nav">\
+        <button class="button button-link pull-left close-picker">取消</button>\
+        <button class="button button-link pull-right close-picker">确定</button>\
+        </header>',
+        cols: [
+            {
+                textAlign: 'center',
+                values: ['5成新', '6成新', '7成新', '8成新', '9成新']
+            }
+        ]
+    });
+    $$.init();
+}
 
 //星级评分
 var rating=(function(){
@@ -160,7 +229,6 @@ var rating=(function(){
             this.add = 1;
         }
     }
-
     //默认参数
     var defaults = {
         mode:'lightEntire',
@@ -199,8 +267,9 @@ var rating=(function(){
         init:init
     }
 })()
-    $('#rating').rating({
-        mode:'lightEntire',
-        num:0
-    })
+//初始化星级评分
+$('#rating').rating({
+    mode:'lightEntire',
+    num:0
+})
 
